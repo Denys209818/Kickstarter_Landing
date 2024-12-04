1078743df03bbe5a1576644b894638518387651c
Denys209818 <deniskravcuk63@gmail.com>
2024-12-04 19:40:38 +0200

    make build

---
+++ index.133bfeea.js
@@ -0,0 +1,2 @@
+document.getElementById("main-form").addEventListener("submit",function(e){e.preventDefault(),e.stopPropagation();for(var t=document.getElementsByClassName("main__form-input"),n=0;n<t.length;n++)t[n].value=""});
+//# sourceMappingURL=index.133bfeea.js.map
